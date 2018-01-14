abab96697bf5310fabcf7ac7904d9d6e06044af8
Przemek <ciemo@wp.pl>
2018-01-14 20:58:16 +0100

    pierwszy commit

---
+++ test.py
@@ -0,0 +1,9 @@
+print ("Jak masz na imie?")
+imie= input()
+print ("czescs " imie)
+print ("Ile masz lat?)
+wiek=input()
+print ("OOO!", wiek, "to ładny wiek")
+print (" zeby zakonczyc program wcisnij 'enter'")
+
+p
